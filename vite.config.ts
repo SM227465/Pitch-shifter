ad8dc137dcf4d98bcfad2e19f321398f68044584
Sourav Mandal <sm227465@gmail.com>
2023-08-23 20:05:12 +0530

    pwa fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,49 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+  manifest: {
+    name: 'Pitch shifter',
+    short_name: 'Pitch shifter',
+    description: 'An app that can shift pitch of audios.',
+    icons: [
+      {
+        src: '/pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon-180x180.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#e8ebf2',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } })],
+  base: './',
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
